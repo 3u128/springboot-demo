b6db11f599811cc7c6c945155aa2475859186d1d
Yevhen Lytviak <ylytviak@gmail.com>
2022-07-08 18:17:56 +0300

    test all pipeline

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,8 +15,8 @@ public class DemoApplication {
 	}
 	
 	@GetMapping("/")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-	return String.format("Hello %s!", name);
+	public String hello(@RequestParam(value = "name", defaultValue = "Hello") String name) {
+	return String.format("World %s!", name);
 	}
 
 }
