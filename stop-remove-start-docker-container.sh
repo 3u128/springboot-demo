f27c8fd5207297dcf1b885131e949dfe7ba87fb0
Yevhen Lytviak <ylytviak@gmail.com>
2022-07-08 19:15:57 +0300

    fix repository in sh script and change run command

--- stop-remove-start-docker-container.sh
+++ stop-remove-start-docker-container.sh
@@ -2,6 +2,6 @@
 DOCKER_NAME=springboot-demo
 docker container stop $DOCKER_NAME
 docker container rm $DOCKER_NAME
-docker rmi $DOCKER_NAME
+docker rmi 3u128/$DOCKER_NAME
 
-docker run -p80:80 --name $DOCKER_NAME -d 3u128/$DOCKER_NAME
+docker run --pull always -p80:80 --name $DOCKER_NAME -d 3u128/$DOCKER_NAME:latest
