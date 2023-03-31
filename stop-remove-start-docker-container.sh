87e01d6708db2dd5bf29d29b788a051d18a051c2
Yevhen Lytviak <yevhen_lytviak@epam.com>
2023-03-31 17:34:34 +0300

    ocker pull and run with parameters

---
+++ stop-remove-start-docker-container.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+DOCKER_NAME=springboot-demo
+docker container stop $DOCKER_NAME
+docker container rm $DOCKER_NAME
+docker rmi 3u128/$DOCKER_NAME
+
+docker run --pull always -p80:80 --name $DOCKER_NAME -d 3u128/$DOCKER_NAME:latest
