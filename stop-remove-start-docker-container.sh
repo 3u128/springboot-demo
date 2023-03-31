a485ae6dc9a3eca774eb54e915694a3a225584ef
Yevhen Lytviak <yevhen_lytviak@epam.com>
2023-03-31 19:08:27 +0300

    8080 change port

--- stop-remove-start-docker-container.sh
+++ stop-remove-start-docker-container.sh
@@ -4,4 +4,4 @@ docker container stop $DOCKER_NAME
 docker container rm $DOCKER_NAME
 docker rmi 3u128/$DOCKER_NAME
 
-docker run --pull always -p80:80 --name $DOCKER_NAME -d 3u128/$DOCKER_NAME:latest
+docker run --pull always -p8080:80 --name $DOCKER_NAME -d 3u128/$DOCKER_NAME:latest
